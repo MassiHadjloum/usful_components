e8dbd6e5486579f45691afda4ffa3781715dad10
MassiHadjloum <massihadjloum@gmail.com>
2023-04-30 19:26:16 +0200

    utils folder

---
+++ src/utils/FAQ.js
@@ -0,0 +1,166 @@
+const HomeFAQ = [
+  {
+    id: 0,
+    title: "Quel est le rôle d'une agence de communication ?",
+    text: "Une agence de communication a pour but d'aider et d'accompagner son client à promouvoir son image, ses créations et ses différents produits ou services sur différents canaux de communication en fonction des besoins et objectifs de l'entreprise.",
+  },
+  {
+    id: 1,
+    title: "Pourquoi faire du Street marketing ?",
+    text: "Les entreprises qui intègrent le street marketing dans leur stratégie marketing profitent des avantages de la communication de proximité. Cette pratique leur permet de transmettre facilement et efficacement des messages clairs et précis à leurs cibles. Grâce à cette méthode, il devient plus facile de gérer la promotion des produits ou des services.",
+  },
+  {
+    id: 2,
+    title: "Pourquoi faire de la distribution de flyers ?",
+    text: "La distribution de prospectus, magazines, flyers et journaux est une façon incontournable de faire passer un message. Que ce soit en boîte aux lettres ou en main-à-main. Ce mode de communication hors-média vous permet d'attirer à vous de potentiels clients, cependant vous devez vous concentrez sur plusieurs facteurs pour assurer une distribution efficace.",
+  },
+  {
+    id: 3,
+    title: "Pourquoi faire du marketing digital pour votre acquisition client ?",
+    text: "Le marketing digital comprend toutes les pratiques liées au marketing qui utilisent des supports et canaux web. Ce sont des canaux moins chers, rapides à mettre en place, mesurables, et qui garantissent l'accès à un très grand éventail de potentiels clients.",
+  },
+
+]
+
+const SMFAQ = [
+  {
+
+    id: 0,
+    title: "Pourquoi faire du Street marketing ?",
+    text: "Les entreprises qui intègrent le street marketing dans leur stratégie marketing profitent des avantages de la communication de proximité. Cette pratique leur permet de transmettre facilement et efficacement des messages clairs et précis à leurs cibles. Grâce à cette méthode, il devient plus facile de gérer la promotion des produits ou des services.",
+  },
+  {
+    id: 1,  
+    title: "Pourquoi faire de la distribution de flyers ?",
+    text: "La distribution de prospectus, magazines, flyers et journaux est une façon incontournable de faire passer un message. Que ce soit en boîte aux lettres ou en main-à-main. Ce mode de communication hors-média vous permet d'attirer à vous de potentiels clients, cependant vous devez vous concentrez sur plusieurs facteurs pour assurer une distribution efficace.",
+  },
+  {
+    id: 2,
+    title: "Pourquoi faire de l'animation ?",
+    text: "Organisez des animations ludiques, originales, participatives dans la rue est un des meilleurs moyens pour augmenter le capital sympathie de votre marque et de solliciter vos prospects au bon endroit, au bon moment !",
+  },
+  {
+    id: 3,
+    title: "Pourquoi faire du dépôt en commerce ?",
+    text: "Distribuer vos tracts directement à travers un emplacement stratégique chez un commerçant est une très bonne opportunité de vous adresser directement à vos clients sans être intrusif.",
+  },
+]
+
+const SMBalFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire de la distribution de flyers ?",
+    text: "La distribution de prospectus, magazines, flyers et journaux est une façon incontournable de faire passer un message. Que ce soit en boîte aux lettres ou en main-à-main. Ce mode de communication hors-média vous permet d'attirer à vous de potentiels clients, cependant vous devez vous concentrez sur plusieurs facteurs pour assurer une distribution efficace.",
+  },
+  {
+    id: 1,
+    title: "De la distribution compatible avec l'écologie ?",
+    text: "Allier distribution de flyers et écologie est tout à fait possible, à vrai dire c'est déjà le cas. Le papier est totalement recyclable. Utiliser du papier recyclé pour produire ses flyers ou autre est bien plus écologique que l’on ne le pense. De plus en limitant le gâchis de papier lors des distribution on fait d'une pierre, deux coups.",
+  },
+]
+
+const SMFlyersFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire de l'animation dans la rue ?",
+    text: "Organisez des animations ludiques, originales, participatives dans la rue est un des meilleurs moyens pour augmenter le capital sympathie de votre marque et de solliciter vos prospects au bon endroit, au bon moment !",
+  },
+  {
+    id: 1,
+    title: "Qu'en est-il de l'affichage sauvage ?",
+    text: "L'affichage sauvage permet de toucher une large cible et plus précisément les riverains d'une zone de chalandise. Un réel avantage qui garantit à votre affiche d'être vue à de multiple reprises et un très bon complément dans une stratégie de communication globale.",
+  },
+]
+
+const SMDepotFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire du dépôt en commerce ?",
+    text: "Distribuer vos tracts directement à travers un emplacement stratégique chez un commerçant est une très bonne opportunité de vous adresser directement à vos clients sans être intrusif.",
+  },
+  {
+    id: 1,
+    title: "Pourquoi faire de l'affichage sauvage ?",
+    text: "L'affichage sauvage permet de toucher une large cible et plus précisément les riverains d'une zone de chalandise. Un réel avantage qui garantit à votre affiche d'être vue à de multiple reprises et un très bon complément dans une stratégie de communication globale.",
+  },
+]
+
+const DigitalFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire du marketing digital pour votre acquisition client ?",
+    text: "Le marketing digital comprend toutes les pratiques liées au marketing qui utilisent des supports et canaux web. Ce sont des canaux moins chers, rapides à mettre en places, mesurables, et qui garantissent l'accès à un très grand éventail de potentiels clients.",
+  },
+  {
+    id: 1,
+    title: "Pourquoi créer un site web ?",
+    text: "Il est quasiment obligatoire pour une entreprise de créer un site internet, pour de nombreuses raisons. Augmenter votre visibilité en étant présent sur le web, présenter vos produits/services, créer un lien direct avec vos clients, de sonder leur opinion. Le plus important reste d'obtenir de nouveaux clients et de faire grossir son chiffre d'affaires.",
+  },
+  {
+    id: 2,
+    title: "Pourquoi faire du SEO ?",
+    text: "Le SEO ou référencement naturel doit être bien travaillé, car il est très important. C'est le seul moyen d'être visible sur les moteurs de recherches gratuitement, par conséquent d'augmenter le trafic de son site, et donc d'attirer de nouveaux clients potentiels. Personne n'ira à la page 10 des résultats de recherche sur un moteur de recherche pour vous trouver désolé ! Cependant, le SEO est un travail sur la durée, c'est le \"marathon\" du marketing digital.",
+  },
+  {
+    id: 3,
+    title: "Pourquoi faire du SEA ?",
+    text: "Le SEA permet d'accélérer l'acquisition client, cependant cela à un coût, il faut mettre de l'argent dans des plateformes telles que Google Ads ou Facebook Ads afin d'attirer des potentiels clients. C'est une pratique qui fonctionne très bien, plus court-termiste que le SEO pour ça que l'on surnomme le SEA le \"sprint\" du digital par opposition à un marathon (les retombées sont plus rapides) !",
+  },
+]
+
+const DigitalWebsiteFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi créer un site web ?",
+    text: "Il est quasiment obligatoire pour une entreprise de créer un site internet, pour de nombreuses raisons. Augmenter votre visibilité en étant présent sur le web, présenter vos produits/services, créer un lien direct avec vos clients, de sonder leur opinion. Le plus important reste d'obtenir de nouveaux clients et de faire grossir son chiffre d'affaire.",
+  },
+  {
+    id: 1,
+    title: "Quelles sont les parties importantes d'un site web ?",
+    text: "Le plus important pour un site internet est le parcours de conversion de son prospect, il doit être simple, clair et efficace. Pour cela il faut faire attention aux landing-pages, aux pages de services ou produits, à la home, globalement l'utilisateur doit pouvoir être converti depuis n'importe quelle page en 3 clics.",
+  },
+]
+
+const DigitalRefFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire du SEO ?",
+    text: "Le SEO ou référencement naturel doit être bien travailler car il est très important. C'est le seul moyen d'être visible sur les moteurs de recherches gratuitement, par conséquent d'augmenter le trafic de son site, et donc d'attirer de nouveaux clients potentiels. Personne n'ira à la page 10 des résultats de recherche sur un moteur de recherche pour vous trouver désolé ! Cependant le SEO est un travail sur la durée, c'est le \"marathon\" du marketing digital.",
+  },
+  {
+    id: 1,
+    title: "Quelles sont les étapes du SEO ? ",
+    text: <>1 - Audit SEO de site internet<br />
+      2 - Audit de visibilité par rapport à la concurrence via des outils marketing (SEMRush par exemple)<br />
+      3 - Recherche de mots clés<br />
+      4 - Optimisation de site web (Technique, Contenu et Popularité - Les 3 pilliers du SEO)<br />
+      5 - Soumission dans les outils de recherche (La Search Console)<br />
+      6 - Vérification de l’inscription<br />
+      7 - Suivi de positionnement dans les outils</>,
+  },
+]
+
+const DigitalPubFAQ = [
+  {
+    id: 0,
+    title: "Pourquoi faire du SEA ?",
+    text: "Le SEA permet d'accélérer l'acquisition client, cependant cela à un coût, il faut mettre de l'argent dans des plateformes telles que Google Ads ou Facebook Ads afin d'attirer des potentiels clients. C'est une pratique qui fonctionne très bien, plus court-termiste que le SEO pour ça que l'on surnomme le SEA le \"sprint\" du digital par opposition à un marathon (les retombées sont plus rapides) !",
+  },
+  {
+    id: 1,
+    title: "Quand faire du SEA ? ",
+    text: "Dès la création du site internet, il est conseillé de commencer à mettre en place une campagne de référencement payant tout en travaillant son référencement naturel : cela vous permettra de gagner rapidement en visibilité.",
+  },
+]
+
+export {
+  HomeFAQ,
+  SMFAQ,
+  SMBalFAQ,
+  SMFlyersFAQ,
+  SMDepotFAQ,
+  DigitalFAQ,
+  DigitalWebsiteFAQ,
+  DigitalRefFAQ,
+  DigitalPubFAQ,
+}
